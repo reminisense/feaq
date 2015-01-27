51058f6467fa92ddbfe4c8dc34a895c14e201ec3
Carl Dalid <carlodalid@gmail.com>
2015-01-27 15:04:40 +0800

    CSD Added Missing UserController Functions 01272015

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -8,4 +8,69 @@
 
 class UserController extends BaseController{
 
+    /*
+     * @author: CSD
+     * @description: verify user data page for first time fb login
+     */
+    public function postVerify(){
+        $fb_id = Input::get('fb_id');
+        if (User::checkFBUser($fb_id)){
+            return Redirect::intended('/user/dashboard');
+        } else {
+            $user = [
+                'first_name'    => Input::get('first_name'),
+                'last_name'     => Input::get('last_name'),
+                'email'         => Input::get('email'),
+                'mobile'        => Input::get('mobile'),
+                'location'      => Input::get('location'),
+                'gender'        => Input::get('gender'),
+                'fb_id'         => Input::get('fb_id'),
+                'fb_url'        => Input::get('fb_url'),
+            ];
+
+            return View::make('user.verify')
+                ->with('user', $user);
+        }
+    }
+
+    /*
+     * author: CSD
+     * @description: store user data from verify user page
+     */
+    public function postStore(){
+        $userData = Input::all();
+
+        $user = new User();
+        $user->first_name = $userData['first_name'];
+        $user->last_name = $userData['last_name'];
+        $user->email = $userData['email'];
+        $user->phone = $userData['mobile'];
+        $user->local_address = $userData['location'];
+        $user->sex = $userData['gender'];
+        $user->fb_id = $userData['fb_id'];
+        $user->fb_url = $userData['fb_url'];
+        $user->save();
+
+        return View::make('dashboard');
+    }
+
+    /*
+     * author: CSD
+     * @description: get page request with fb_id parameter
+     */
+    public function getFacebookUser($fb_id){
+        if (User::checkFBUser($fb_id)){
+            return json_encode(array('succes' => 1));
+        } else {
+            return json_encode(array('success' => 0));
+        }
+    }
+
+    /*
+     * author: CSD
+     * @description: get dashboard page
+     */
+    public function getDashboard(){
+        return View::make('dashboard');
+    }
 }
