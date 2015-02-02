3c164464cbcb8d6dc2be2b703cf018f402452226
Carl Dalid <carlodalid@gmail.com>
2015-02-02 15:28:59 +0800

    CSD User and Business Setup Form Validation 02022015

--- public/js/dashboard/dashboard.js
+++ public/js/dashboard/dashboard.js
@@ -31,30 +31,103 @@ $(document).ready(function(){
     });
 
     $('#start_queuing').on('click', function(){
-        $.ajax({
-            url: $('#verification_form').attr('action'),
-            type: 'POST',
-            dataType: 'text json',
-            data: $('#verification_form').serialize(),
-            success: function(response){
-                $('#verifyUser').modal('hide');
-                $('#setupBusiness').modal();
-                $('#setupBusiness').prepend('<div class="modal-backdrop fade in" style="height: ' + $( window ).height() + 'px"></div>');
-            }
-        });
+        var errorMessage = '';
+        if ($('#first_name').val() == ""){
+            errorMessage = "First Name field is required. ";
+        }
+
+        if ($('#last_name').val() == ""){
+            errorMessage = errorMessage + "Last Name field is required. ";
+        }
+
+        if ($('#email').val() == ""){
+            errorMessage = errorMessage + "Email field is required. ";
+        }
+
+        if ($('#mobile').val() == "" || $('#mobile').val() == "0"){
+            errorMessage = errorMessage + "Mobile field is required. ";
+        }
+
+        if ($('#user_location').val() == ""){
+            errorMessage = errorMessage + "Location field is required. ";
+        }
+
+        if (!isEmail($('#email').val())){
+            errorMessage = errorMessage + "Invalid Email field input. ";
+        }
+
+        if (errorMessage == ""){
+            $.ajax({
+                url: $('#verification_form').attr('action'),
+                type: 'POST',
+                dataType: 'text json',
+                data: $('#verification_form').serialize(),
+                success: function(response){
+                    $('#verifyUser').modal('hide');
+                    $('#setupBusiness').modal();
+                    $('#setupBusiness').prepend('<div class="modal-backdrop fade in" style="height: ' + $( window ).height() + 'px"></div>');
+                }
+            });
+        } else {
+            $('#verifyError').text(errorMessage);
+            $('#verifyError').fadeIn( 400 );
+        }
     });
 
     $('#submit_business').on('click', function(){
-        $.ajax({
-            url: $('#add_business_form').attr('action'),
-            type: 'POST',
-            dataType: 'text json',
-            data: $('#add_business_form').serialize(),
-            success: function(response){
-                $('#setupBusiness').modal('hide');
-                window.location.href = '/';
-            }
-        });
+        var errorMessage = '';
+        if ($('#business_name').val() == ""){
+            errorMessage = "Business Name field is required. ";
+        }
+
+        if ($('#business_location').val() == ""){
+            errorMessage = errorMessage + "Business Address field is required. ";
+        }
+
+        if ($('#time_open').val() == ""){
+            errorMessage = errorMessage + "Time Open field is required. ";
+        } else if (!isValidTime($('#time_open').val())) {
+            errorMessage = errorMessage + "Invalid Time Open field input. ";
+        }
+
+        if ($('#time_close').val() == ""){
+            errorMessage = errorMessage + "Time Close field is required. ";
+        } else if (!isValidTime($('#time_close').val())) {
+            errorMessage = errorMessage + "Invalid Time Open field input. ";
+        }
+
+        if ($('#industry').val() == ""){
+            errorMessage = errorMessage + "Industry field is required. ";
+        }
+
+        if ($('#num_terminals').val() == ""){
+            errorMessage = errorMessage + "Terminal Number field is required. ";
+        }
+
+        if ($('#queue_limit').val() == ""){
+            errorMessage = errorMessage + "Queue Number Limit field is required. ";
+        } else if (!$.isNumeric($('#queue_limit').val())){
+            errorMessage = errorMessage + "Invalid Queue Limit field input. ";
+        } else if ($('#queue_limit').val() < 0 && $.isNumeric($('#queue_limit').val())){
+            errorMessage = errorMessage + "Invalid Queue Limit field input. ";
+        }
+
+        if (errorMessage == ""){
+            $.ajax({
+                url: $('#add_business_form').attr('action'),
+                type: 'POST',
+                dataType: 'text json',
+                data: $('#add_business_form').serialize(),
+                success: function(response){
+                    $('#setupBusiness').modal('hide');
+                    window.location.href = '/';
+                }
+            });
+        } else {
+            $('#setupError').text(errorMessage);
+            $('#setupError').fadeIn( 400 );
+        }
+
     });
 
     $('#add_business').on('click', function(){
@@ -64,5 +137,17 @@ $(document).ready(function(){
         $('#setupBusiness').modal('show');
         $('#setupBusiness').prepend('<div class="modal-backdrop fade in" style="height: ' + $( window ).height() + 'px"></div>');
     });
+
+    $("#mobile").intlTelInput();
+
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+
+    function isValidTime(time){
+        var regex = /^(0?[1-9]|1[012])(:[0-5]\d) [APap][mM]$/;
+        return regex.test(time);
+    }
 
 });
