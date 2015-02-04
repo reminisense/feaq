23f145c8c487f8db18aa5dd348fc31ba3b239020
rdhdia <ruffy.heredia@gmail.com>
2015-02-05 07:43:40 +0800

    RDH Added Links to Profile Dropdown - Added a link back to the Dashboard - Added Social Media links

--- app/views/broadcast.blade.php
+++ app/views/broadcast.blade.php
@@ -47,13 +47,12 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hello {{Auth::user()->first_name}}! <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
+                        <li><a href="{{URL::to('/')}}">Dashboard</a></li>
                         <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
+                        <li class="dropdown-header">Connect with us!</li>
+                        <li><a href="{{URL::to('https://www.facebook.com/theFeatherQ')}}">Facebook</a></li>
+                        <li><a href="{{URL::to('https://www.twitter.com/thefeatherq')}}">Twitter</a></li>
+                        <li><a href="{{URL::to('https://plus.google.com/101914769293976664743')}}">Google+</a></li>
                     </ul>
                 </li>
 
