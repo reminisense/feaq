80100679a02d61251ac2bf8d5a31fb5c7f7c3605
rdhdia <ruffy.heredia@gmail.com>
2015-02-05 08:08:57 +0800

    RDH Edited Texthint on Edit Business for Description - As described

--- app/views/modals/business/edit-business-modal.blade.php
+++ app/views/modals/business/edit-business-modal.blade.php
@@ -46,7 +46,7 @@
                                 </div>
                                 <div class="col-md-12">
                                     <small>Description</small>
-                                    <textarea rows="10" class="form-control" placeholder="Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."></textarea>
+                                    <textarea rows="10" class="form-control" placeholder="Add A Description Of Your Business Here! Try and talk about what your business does, how it started, and how valuable it is to your customers' lives! When we talk about our business, we can definitely say a whole lot about it! So don't hesitate to write it down here."></textarea>
                                 </div>
 
                             </div>
