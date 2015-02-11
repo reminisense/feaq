14f6d917a26c05b7e6de53ca009a4679bfe1f192
rdhdia <ruffy.heredia@gmail.com>
2015-02-11 20:06:09 +0800

    RDH Added database details for Production - As described

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
             'host'      => 'localhost',
             'database'  => 'feaq',
             'username'  => 'forge',
-            'password'  => 'TkLKr5ysCF9DQfJuVzx7',
+            'password'  => 'jE8Gf9whVlHjXeIpCYiM',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
