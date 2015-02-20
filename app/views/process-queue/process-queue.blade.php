1ea6a634a9efcc17c1161b11e99015c006b960e5
Aunne Arzadon <nodazra@gmail.com>
2015-02-20 14:38:30 +0800

    ARA Next button spam ARA 02202015 Bug fix for FEAQ -161 Updated blade template for next called numbers

--- app/views/process-queue/process-queue.blade.php
+++ app/views/process-queue/process-queue.blade.php
@@ -77,10 +77,10 @@
                             </div>
                         </form>
                         <!-- called numbers -->
-                        <table class="table">
+                        <table class="table" id="called-numbers">
                             <thead></thead>
                             <tbody>
-                            <tr ng-repeat="number in called_numbers">
+                            <tr ng-repeat="number in called_numbers" data-tnumber="@{{ number.transaction_number }}">
                                 <th scope="row">@{{ number.priority_number }}</th>
                                 <!-- if this terminal -->
                                 <td ng-if="number.terminal_id == terminal_id">
