a376167ebe8443d08f8ba9940d4b621ebd8b5f5c
Aunne Arzadon <nodazra@gmail.com>
2015-03-09 17:56:40 +0800

    ARA Show Process Queue Number Details ARA 03092015 Added feature to show modal when clicking a priority_number

---
+++ app/views/modals/process-queue/priority-number-details-modal.blade.php
@@ -0,0 +1,53 @@
+<!-- modal -->
+<div class="modal fade" id="priority-number-modal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h3 class="modal-title" id=""></h3>
+            </div>
+            <div class="modal-body">
+                <table>
+                    <tr>
+                        <td class="row">
+                            <div class="col-lg-1"><h5>Priority Number: </h5></div>
+                        </td>
+                        <td class="row">
+                            <div class="col-lg-11" id="priority-number-number"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="row">
+                            <div class="col-lg-1"><h5>Name: </h5></div>
+                        </td>
+                        <td class="row">
+                            <div class="col-lg-11" id="priority-number-name"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="row">
+                            <div class="col-lg-1"><h5>Phone: </h5></div>
+                        </td>
+                        <td class="row">
+                            <div class="col-lg-11" id="priority-number-phone"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="row">
+                            <div class="col-lg-1"><h5>Email: </h5></div>
+                        </td>
+                        <td>
+                            <div class="col-lg-11" id="priority-number-email"></div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-orange btn-lg" data-dismiss="modal" aria-label="Close">CLOSE</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--eo modal-->
