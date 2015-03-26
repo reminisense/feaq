b434d43669d1eb7fa93b2827dd2a590c55018a22
Carl Dalid <carlodalid@gmail.com>
2015-03-26 15:40:43 +0800

    CSD Updated Edit Business Modal 03262015 - added scroll for business ads tab - disabled upload and embed button when no file is selected

--- app/views/modals/business/edit-business-modal.blade.php
+++ app/views/modals/business/edit-business-modal.blade.php
@@ -9,7 +9,7 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h3 class="modal-title" id="myModalLabel">Edit Business</h3> {{--CSD Goob job finding this!--}}
             </div>
-            <div class="modal-body">
+            <div class="modal-body" id="edit_business_body">
                 <form class="">
                     <div class="form-group row">
 
