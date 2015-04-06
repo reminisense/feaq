9e7a8cbcaad8836b1731eaad64f5093ba4b1cad5
Jonas Clyde Almocera <jonas@reminisense.com>
2015-04-06 15:01:54 +0800

    JCA Carousel Default Ads 04062015 -Added carousel feature on default ads images.

--- app/views/business-broadcast.blade.php
+++ app/views/business-broadcast.blade.php
@@ -66,8 +66,30 @@
       </div>
     </div>
       <div class="col-md-6 ads" style="@{{ ad_display }}">
-          <img class="img-responsive mb30" src="@{{ ad_image }}" id="image-ad"/>
-          <div id="internet-tv"></div>
+        <div id="ad-image-container">
+          <img class="img-responsive mb30" src="@{{ ad_image }}" id="image-ad" style="@{{ ad_display_upload }}"/>
+            <div id="fqCarousel" class="carousel slide" data-ride="carousel" style="@{{ ad_display_default }}">
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img src="/images/ads.jpg" alt="Ad1">
+                </div>
+                <div class="item">
+                  <img src="/images/broadcast1.jpg" alt="Ad2">
+                </div>
+                <div class="item">
+                  <img src="/images/broadcast2.jpg" alt="Ad3">
+                </div>
+                <div class="item">
+                  <img src="/images/broadcast3.jpg" alt="Ad4">
+                </div>
+                <div class="item">
+                  <img src="/images/broadcast4.jpg" alt="Ad5">
+                </div>
+              </div>
+            </div>
+          </div>
+        <div id="internet-tv"></div>
       </div>
     <div class="col-md-@{{ colsize }}">
       <div class="boxed mb20 bcast-big">
