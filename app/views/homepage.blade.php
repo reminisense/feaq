0b64e9d06b3fd425671ea7bdf7ccd1b1d65ee2a1
Carl Dalid <carlodalid@gmail.com>
2015-04-13 17:22:53 +0800

    CSD Typo on Login 03132015 - fixed typo on Log In text

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -45,7 +45,7 @@
         <ul class="nav navbar-nav navbar-right">
           <li><a class="feats" href="#features">FeatherQ Features</a></li>
           <li><a class="start" href="#getting-started">Getting Started</a></li>
-          <li style="margin-left:10px;"><a href="" class="btn btn-blue btn-fb" ng-click="login()" role="button"><img src="img/icon-fb.png">Login In</a></li>
+          <li style="margin-left:10px;"><a href="" class="btn btn-blue btn-fb" ng-click="login()" role="button"><img src="img/icon-fb.png">Log In</a></li>
         </ul>
       </div><!--/.nav-collapse -->
     </div>
