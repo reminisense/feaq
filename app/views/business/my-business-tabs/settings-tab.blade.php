2702c217964e99645eadfb33f5cb330c9be05048
rodelmaranon <rodel.maranon@gmail.com>
2015-04-15 17:41:30 +0800

    RBM My Business Page - Layout Fixes

--- app/views/business/my-business-tabs/settings-tab.blade.php
+++ app/views/business/my-business-tabs/settings-tab.blade.php
@@ -1,5 +1,5 @@
 <h5>QUEUE SETTINGS</h5>
-<table class="table">
+<table class="table settings">
     <tbody>
     {{--RDH Removed Other Queue Settings Since These Do Not Apply To This Release--}}
     {{--<tr>
@@ -7,68 +7,69 @@
         <td><input class="mb0 form-control" type="text" placeholder="@{{ number_start }}"></td>
     </tr>--}}
     <tr>
-        <td>Number Limit</td>
+        <td>
+            <p class="title">Number Limit</p>
+        </td>
         <td><input class="mb0 form-control" type="text" value="@{{ queue_limit }}" ng-model="queue_limit" ></td>  {{--RDH Added queue_limit to Edit Business Page--}}
     </tr>
     <tr>
-        <td>Show Only Numbers Issued By Terminal</td>
+        <td>
+            <p class="title">Show Only Numbers Issued By Terminal</p>
+        </td>
         <td><input type="checkbox" ng-model="terminal_specific_issue"></td> {{--ARA Terminal-specific issue numbers--}}
     </tr>
     <tr>
         <td>
-            <strong>*</strong> Frontline SMS Secret
+            <p class="title">* Frontline SMS Secret
             <a href="https://frontlinecloud.zendesk.com/entries/28395408-Using-the-WebConnection-API-to-send-messages" target="_blank">
                 <span class="glyphicon glyphicon-question-sign" title="How to create a Web Connection in Frontlinesms"></span>
             </a>
+            </p>
         </td>
         <td><input class="mb0 form-control" type="password" value="@{{ frontline_secret }}" ng-model="frontline_secret" ></td>
     </tr>
     <tr>
         <td>
-            <strong>*</strong> Frontline SMS URL
+            <p class="title">* Frontline SMS URL
             <a href="https://frontlinecloud.zendesk.com/entries/28395408-Using-the-WebConnection-API-to-send-messages" target="_blank">
                 <span class="glyphicon glyphicon-question-sign" title="How to create a Web Connection in Frontlinesms"></span>
             </a>
+            </p>
         </td>
         <td><input class="mb0 form-control" type="text" value="@{{ frontline_url }}" ng-model="frontline_url" ></td>
     </tr>
-    <tr>
-        <td>
-            SMS Notification Settings
-            <span class="glyphicon glyphicon-info-sign" style="color:#337ab7;" title="When to notify users via SMS."></span>
-        </td>
-    </tr>
+
     <tr>
         <td colspan="2">
-            <input type="checkbox"  ng-model="sms_current_number">
-            Priority Number is called
+            <div class="col-md-12">
+                <p class="title">SMS Notification Settings
+                <span class="glyphicon glyphicon-info-sign" style="color:#337ab7;" title="When to notify users via SMS."></span></p>
+            </div>
+            <div class="col-md-2">
+                <input type="checkbox" ng-model="sms_current_number">
+                Priority Number is called
+            </div>
+            <div class="col-md-2">
+                <input type="checkbox"  ng-model="sms_1_ahead">
+                Priority Number is next in queue
+            </div>
+            <div class="col-md-2">
+                <input type="checkbox"  ng-model="sms_5_ahead">
+                 5 Numbers ahead in queue
+            </div>
+            <div class="col-md-2">
+                 <input type="checkbox"  ng-model="sms_10_ahead">
+                             10 Numbers ahead in queue
+                        </div>
+            <div class="col-md-2">
+                  <input id="input_sms_field" type="text" ng-model="input_sms_field">
+                  <input type="checkbox"  ng-model="sms_blank_ahead">
+                  Numbers ahead in queue.
+            </div>
+
         </td>
     </tr>
-    <tr>
-        <td>
-            <input type="checkbox"  ng-model="sms_1_ahead">
-            Priority Number is next in queue
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type="checkbox"  ng-model="sms_5_ahead">
-            5 Numbers ahead in queue
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type="checkbox"  ng-model="sms_10_ahead">
-            10 Numbers ahead in queue
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input id="input_sms_field" type="text" ng-model="input_sms_field">
-            <input type="checkbox"  ng-model="sms_blank_ahead">
-            Numbers ahead in queue.
-        </td>
-    </tr>
+
 
     {{--<tr>
         <td>Loop numbers automatically.</td>
