4cc478d510d19682a94a12cff1150da3a09d0355
Jonas Clyde Almocera <jonas@reminisense.com>
2015-04-15 22:09:25 +0800

    JCA Public Broadcast Screen Default Images 042152015 -Changed implementation on Public Broadcast screen when no images are uploaded

--- app/views/broadcast/default/public-ads-master.blade.php
+++ app/views/broadcast/default/public-ads-master.blade.php
@@ -3,12 +3,18 @@
         <div id="fqCarousel" class="carousel slide" data-ride="carousel">
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img src="{{ $ad_src }}" alt="Ad1">
-                </div>
-                <div class="item">
-                    <img src="/images/broadcast1.jpg" alt="Ad2">
-                </div>
+                @if ($ad_src != '')
+                    <div class="item active">
+                        <img src="{{ $ad_src }}" alt="Ad1">
+                    </div>
+                    <div class="item">
+                        <img src="/images/broadcast1.jpg" alt="Ad2">
+                    </div>
+                @else
+                    <div class="item active">
+                        <img src="/images/broadcast1.jpg" alt="Ad2">
+                    </div>
+                @endif
                 <div class="item">
                     <img src="/images/broadcast2.jpg" alt="Ad3">
                 </div>
