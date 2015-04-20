bd245cf8667295fd11ea7f57f965f62798d609ae
Aunne Arzadon <nodazra@gmail.com>
2015-04-20 18:42:32 +0800

    ARA Business search ARA 04202015 Added fields in business search business cards

--- app/views/user/dashboardnew.blade.php
+++ app/views/user/dashboardnew.blade.php
@@ -73,8 +73,12 @@
                     <p class="title">{{ $business['name'] }}</p>
                     <p class="address">{{ $business['local_address'] }}</p>
                     <div class="statuses">
-                      <p><span class="icon-lineq"></span> Business Hours: {{ $business['open_time'] }} - {{ $business['close_time'] }} <span class="icon-busy"></span></p>
-                      <p><span class="icon-waittime"></span> Waiting Time: {{ $business['waiting_time'] }} </p>
+                      <p><span class="icon-lineq"></span> Business Hours: <span class="pull-right">{{ $business['open_time'] }} - {{ $business['close_time'] }}</span><span class="icon-busy"></span> </p>
+                      <p><span class="icon-waittime"></span> Waiting Time: <span class="pull-right">{{ $business['waiting_time'] }}</span> </p>
+                      <p> Last Number Called: <span class="pull-right">{{ $business['last_number_called'] }}</span> </p>
+                      <p> Next Available Number: <span class="pull-right">{{ $business['next_available_number'] }}</span> </p>
+                      <p> Calling: <span class="pull-right">{{ $business['is_calling'] }}</span> </p>
+                      <p> Issuing: <span class="pull-right">{{ $business['is_issuing'] }}</span> </p>
                     </div>
                   </div>
                   </a>
