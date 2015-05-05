cba01b49e64bf8476e1214364c0b124ab3cb82f5
Aunne Arzadon <nodazra@gmail.com>
2015-05-05 13:35:39 +0800

    ARA Merged PAG-Personalized_business_searchhistory ARA 05052015 -Merged paul's watchdog functions with mine. -Added data querying functions to get user data

--- app/models/Watchdog.php
+++ app/models/Watchdog.php
@@ -8,16 +8,37 @@
 
 class Watchdog extends Eloquent {
 
-  protected $table = 'watchdog';
-  protected $primaryKey = 'log_id';
-  public $timestamps = false;
+    protected $table = 'watchdog';
+    protected $primaryKey = 'log_id';
+    public $timestamps = false;
 
-  public static function createRecord($val = array()) {
-    Watchdog::insert($val);
-  }
+    public static function createRecord($val = array()) {
+        return Watchdog::insertGetId($val);
+    }
 
-  public static function addViewHistory($data){
-    return Watchdog::insertGetId($data);
-  }
+    public static function getUserRecords($user_id = null){
+        $user_data = [];
+        if($user_id){
+            $results = Watchdog::where('user_id', '=', $user_id)->get();
+        }else{
+            $results = Watchdog::all();
+        }
 
+        foreach($results as $index => $data){
+            $user_data[$index] = unserialize($data->value);
+            $user_data[$index]['user_id'] = $data->user_id;
+            $user_data[$index]['action_type'] = $data->action_type;
+        }
+        return $user_data;
+    }
+
+    public static function queryUserInfo($keyword, $user_id = null){
+        $user_data = Watchdog::getUserRecords($user_id);
+        $values = [];
+        foreach($user_data as $data){
+            $values[] = $data[$keyword];
+        }
+        $numbers = array_count_values($values);
+        return $numbers;
+    }
 }
