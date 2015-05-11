e98262cd2f18f2361fd247e4442c0b92cba39a0a
Carl Dalid <carlodalid@gmail.com>
2015-05-11 10:44:38 +0800

    CSD Contact Form Validation 05112015 - added form validation for business contact form

--- app/views/modals/broadcast/contact-business-modal.blade.php
+++ app/views/modals/broadcast/contact-business-modal.blade.php
@@ -9,29 +9,29 @@
             </div>
             <div class="modal-body">
                 <div class="clearfix">
-                    <div id="message-notif" class="alert alert-success" style="display: none;" role="alert"></div>
+                    <div id="message-notif" class="alert alert-success" style="display: none; text-align: center;" role="alert"></div>
                     <form action="{{ url('/broadcast/business-message') }}" class="navbar-form navbar-left" name="contact_business_form">
                         <div class="form-group row">
                             <div class="col-md-3">
-                                <label>Name</label>
+                                <label>Name <span class="req">*</span></label>
                             </div>
                             <div class="col-md-9">
                                 <input type="text" class="form-control" id="contactname" required />
                             </div>
                             <div class="col-md-3">
-                                <label>Email</label>
+                                <label>Email <span class="req">*</span></label>
                             </div>
                             <div class="col-md-9">
                                 <input type="email" class="form-control" id="contactemail" required />
                             </div>
                             <div class="col-md-3">
-                                <label>Mobile Number</label>
+                                <label>Mobile Number <span class="req">*</span></label>
                             </div>
                             <div class="col-md-9">
                                 <input type="mobile" class="form-control" id="contactmobile" required />
                             </div>
                             <div class="col-md-3">
-                                <label>Message</label>
+                                <label>Message <span class="req">*</span></label>
                             </div>
                             <div class="col-md-9">
                                 <textarea class="form-control" rows="5" id="contactmessage" placeholder="Write your message here..." required ></textarea>
