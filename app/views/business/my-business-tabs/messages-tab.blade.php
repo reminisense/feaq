247fa67ba470274b5b2c22f1a00cb9569aaf832d
Carl Dalid <carlodalid@gmail.com>
2015-05-22 09:59:09 +0800

    CSD Fixed Template for Messaging Tab on Firefox 05222015 - fixed template on messaging tab for firebox browser

--- app/views/business/my-business-tabs/messages-tab.blade.php
+++ app/views/business/my-business-tabs/messages-tab.blade.php
@@ -12,24 +12,26 @@
             <div class="preview-container">
                 <div class="message-preview" style="display: none;">
                     <div class="message-reply">
+                            <div class="col-md-2 mt20"><label>Send via:</label></div>
+                            <div class="col-md-2 mt20"><input id="sendbyphone" type="checkbox" ng-model="sendby.phone" ng-true-value="'phone'" ng-false-value="'0'" ng-init="sendby.phone='0'"><label class="optionlabel">Phone</label></div>
+                            <div class="col-md-4" id="select-phone-div">
+                                <select class="form-control phone-select" ng-model="business_reply_form.pick_number" ng-init="business_reply_form.pick_number">
+                                    <option value="0">- Select A Number -</option>
+                                    <option ng-repeat="number in number_list" value="@{{ number }}">@{{ number }}</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4"></div>
 
-                        <div class="col-md-2 mt20"><label>Send via:</label></div>
-                        <div class="col-md-2 mt20"><input id="sendbyphone" type="checkbox" ng-model="sendby.phone" ng-true-value="'phone'" ng-false-value="'0'" ng-init="sendby.phone='0'"><label class="optionlabel">Phone</label></div>
-                        <div class="col-md-4" id="select-phone-div">
-                            <select class="form-control phone-select" ng-model="business_reply_form.pick_number" ng-init="business_reply_form.pick_number">
-                                <option value="0">- Select A Number -</option>
-                                <option ng-repeat="number in number_list" value="@{{ number }}">@{{ number }}</option>
-                            </select>
-                        </div>
-                        <div class="col-md-2"></div>
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                             <div role="alert" class="alert alert-info" style="margin-top: 10px; padding: 0px 10px; font-size: 12px;">Your reply will be sent via email. Please select Phone if you wish to send including SMS.</div>
-                        </div>
-                        <form ng-submit="sendBusinessReply()">
-                            <textarea id="sendreplytext" class="form-control" rows="5" placeholder="Write a reply..." ng-model="business_reply_form.message_reply" required></textarea>
-                            <button id="sendreply" type="submit" class="btn btn-default btn-orange">Send Reply</button>
-                        </form>
+                            <div class="col-md-12">
+                                 <div role="alert" class="alert alert-info" style="margin-top: 10px; padding: 0px 10px; font-size: 12px;">Your reply will be sent via email. Please select Phone if you wish to send including SMS.</div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <form ng-submit="sendBusinessReply()">
+                                    <textarea id="sendreplytext" class="form-control" rows="5" placeholder="Write a reply..." ng-model="business_reply_form.message_reply" required></textarea>
+                                    <button id="sendreply" type="submit" class="btn btn-default btn-orange">Send Reply</button>
+                                </form>
+                            </div>
                     </div>
                 </div>
             </div>
