9e999d192e29e172658c48a11e925febc2051798
Aunne Arzadon <nodazra@gmail.com>
2015-05-26 14:34:04 +0800

    ARA Messages in process queue ARA 02262015 Added UI for messages in process queue page

--- app/views/modals/process-queue/priority-number-details-modal.blade.php
+++ app/views/modals/process-queue/priority-number-details-modal.blade.php
@@ -9,40 +9,45 @@
                 <h3 class="modal-title" id=""></h3>
             </div>
             <div class="modal-body">
-                <table>
-                    <tr>
-                        <td class="row">
-                            <div class="col-lg-1"><h5>Priority Number: </h5></div>
-                        </td>
-                        <td class="row">
-                            <div class="col-lg-11" id="priority-number-number"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="row">
-                            <div class="col-lg-1"><h5>Name: </h5></div>
-                        </td>
-                        <td class="row">
-                            <div class="col-lg-11" id="priority-number-name"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="row">
-                            <div class="col-lg-1"><h5>Phone: </h5></div>
-                        </td>
-                        <td class="row">
-                            <div class="col-lg-11" id="priority-number-phone"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="row">
-                            <div class="col-lg-1"><h5>Email: </h5></div>
-                        </td>
-                        <td>
-                            <div class="col-lg-11" id="priority-number-email"></div>
-                        </td>
-                    </tr>
-                </table>
+                <ul id="pmore-tab" class="nav nav-tabs nav-justified">
+                    <li class="active"><a href="#details" data-toggle="tab">DETAILS</a></li>
+                    <li><a href="#messages" data-toggle="tab" >MESSAGES</a></li>
+                </ul>
+                <div class="clearfix tab-content">
+                    <div class="tab-pane fade active in" id="details">
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-5"><h5>Priority Number: </h5></div>
+                            <div class="col-md-6"><h5 id="priority-number-number"></h5></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-5"><h5>Name: </h5></div>
+                            <div class="col-md-6"><h5 id="priority-number-name"></h5></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-5"><h5>Phone: </h5></div>
+                            <div class="col-md-6"><h5 id="priority-number-phone"></h5></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-1"></div>
+                            <div class="col-md-5"><h5>Email: </h5></div>
+                            <div class="col-md-6"><h5 id="priority-number-email"></h5></div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade in" id="messages">
+                        <div class="row">
+                            <div class="col-md-12 text-center"><h5>Messages from <span>Name</span></h5></div>
+                            <div class="col-md-12"></div>
+                            <div class="col-md-12">
+                                <h5>Send A Message to <span>Phone number</span></h5>
+                                <textarea class="form-control" rows="5" placeholder="Write a message..." style="resize: none;"></textarea>
+                                <button>Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-orange btn-lg" data-dismiss="modal" aria-label="Close">CLOSE</button>
