89e68d39ec5a04bd81b2402d9000a77be41e0879
Carl Dalid <carlodalid@gmail.com>
2015-05-27 11:13:56 +0800

    CSD Fixed CSS on Advertisement Tab 05272015 - fixed styles and template on advertisement tab

--- app/views/business/my-business-tabs/advertisements-tab.blade.php
+++ app/views/business/my-business-tabs/advertisements-tab.blade.php
@@ -32,64 +32,52 @@
         <h5>IMAGE OR VIDEO
             {{--<small>Choose whether to put an image or video in your broadcast screen.</small>--}}
         </h5>
-
-        <div class="">
             <div class="clearfix">
                     <label><input type="radio" name="ad_type" value="image" ng-model="ad_type" ng-change="adType(ad_type, business_id)"> <strong>Image</strong></label><label><input style="margin-left: 10px;" type="radio" name="ad_type" value="video" ng-model="ad_type" ng-change="adType(ad_type, business_id)"> <strong>Video</strong></label>
             </div>
             <br>
-                {{--<div class="col-md-6 col-sm-12">
-                     <div role="alert" class="alert alert-info" style="padding: 10px 10px; font-size: 12px;">Choose whether to put an image or video in your broadcast screen.</div>
-                </div>--}}
-                <div class="">
-                    <div class="">
-                        <div class="clearfix" id="image-adtype">
-                            <div class="clearfix">
-                                                            <form action="/advertisement/upload-image" method="POST" enctype="multipart/form-data" id="ad-image-uploader">
-                                                                <div class="form-group">
-                                                                    <label for="exampleInputFile"><strong>Choose Image to Upload:</strong></label>
-                                                                    <input name="ad_image" type="file" id="ad-image" required>
-                                                                    <em class="help-block">Upload images with .jpg, .png file format. Best resolution is 800 x 803 pixels.</em>
-                                                                    <div class="alert alert-success" id="adimage-success" style="display: none;">Success! <strong><a href="/broadcast/business/@{{ business_id }}" target="_blank">View Broadcast Page</a></strong></div>
-                                                                    <div class="alert alert-danger" id="adimage-danger" style="display: none;">Oops! Something went wrong.</div>
-                                                                </div>
-                                                                <div class="form-group">
-                                                                    <button type="submit" class="btn btn-orange" id="image-submit-btn" ng-click="adImageUpload(business_id)" style="color: #ffffff;"><span class="glyphicon glyphicon-check"></span> UPLOAD</button>
-                                                                    <button id="loading-img" style="display:none;" class="btn btn-orange btn-disabled"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...</button>
-                                                                </div>
-                                                            </form>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="col-md-12" style="text-align: center;">
-                                    <img src="/" id="ad-preview" style="width: 100%;">
-                                </div>
-                            </div>
-
+            {{--<div class="col-md-6 col-sm-12">
+                 <div role="alert" class="alert alert-info" style="padding: 10px 10px; font-size: 12px;">Choose whether to put an image or video in your broadcast screen.</div>
+            </div>--}}
+            <div class="clearfix" id="image-adtype">
+                <div class="col-md-12">
+                    <form action="/advertisement/upload-image" method="POST" enctype="multipart/form-data" id="ad-image-uploader">
+                        <div class="form-group">
+                            <label for="exampleInputFile"><strong>Choose Image to Upload:</strong></label>
+                            <input name="ad_image" type="file" id="ad-image" required>
+                            <em class="help-block">Upload images with .jpg, .png file format. Best resolution is 800 x 803 pixels.</em>
+                            <div class="alert alert-success" id="adimage-success" style="display: none;">Success! <strong><a href="/broadcast/business/@{{ business_id }}" target="_blank">View Broadcast Page</a></strong></div>
+                            <div class="alert alert-danger" id="adimage-danger" style="display: none;">Oops! Something went wrong.</div>
                         </div>
-                        <div id="video-adtype">
-                            <div class="col-md-6">
-                                <div class="col-md-12" style="text-align: center;">
-                                    <iframe width="100%" height="315" src="" id="advideo-preview"></iframe>
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <form ng-submit="adVideoEmbed(business_id)" id="ad-video-uploader">
-                                    <div class="form-group">
-                                        <label for="exampleInputFile"><strong>Paste YouTube URL:</strong></label>
-                                        <input ng-model="ad_video" type="text" id="ad-video" required>
-                                        <div class="alert alert-success" id="advideo-success" style="display: none;">Success! <strong><a href="/broadcast/business/@{{ business_id }}" target="_blank">View Broadcast Page</a></strong></div>
-                                        <div class="alert alert-danger" id="advideo-danger" style="display: none;">Oops! Something went wrong.</div>
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-orange" id="image-submit-btn" style="color: #ffffff;"><span class="glyphicon glyphicon-check"></span> EMBED</button>
-                                        <button id="loading-img-2" style="display:none;" class="btn btn-orange btn-disabled"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...</button>
-                                    </div>
-                                </form>
-                            </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-orange" id="image-submit-btn" ng-click="adImageUpload(business_id)" style="color: #ffffff;"><span class="glyphicon glyphicon-check"></span> UPLOAD</button>
+                            <button id="loading-img" style="display:none;" class="btn btn-orange btn-disabled"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
-        </div>
+                <div class="col-md-12" style="text-align: center;">
+                    <img src="/" id="ad-preview" style="width: 100%;">
+                </div>
+            </div>
+            <div class="clearfix" id="video-adtype">
+                <div class="col-md-12">
+                    <form ng-submit="adVideoEmbed(business_id)" id="ad-video-uploader">
+                        <div class="form-group">
+                            <label for="exampleInputFile"><strong>YouTube URL:</strong></label>
+                            <input ng-model="ad_video" type="text" id="ad-video" required>
+                            <div class="alert alert-success" id="advideo-success" style="display: none;">Success! <strong><a href="/broadcast/business/@{{ business_id }}" target="_blank">View Broadcast Page</a></strong></div>
+                            <div class="alert alert-danger" id="advideo-danger" style="display: none;">Oops! Something went wrong.</div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-orange" id="image-submit-btn" style="color: #ffffff;"><span class="glyphicon glyphicon-check"></span> EMBED</button>
+                            <button id="loading-img-2" style="display:none;" class="btn btn-orange btn-disabled"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-12" style="text-align: center;">
+                    <iframe width="100%" height="315" src="" id="advideo-preview"></iframe>
+                </div>
+            </div>
         </div>
     </div>
 
