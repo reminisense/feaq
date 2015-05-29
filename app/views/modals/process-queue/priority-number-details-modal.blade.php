9882fbc0bc7ea8e2f41d8cc47e2f2293be58e8da
rodelmaranon <rodel.maranon@gmail.com>
2015-05-29 13:33:17 +0800

    RBM processqueue and modal fixes =05292015 -processqueue page -modal fixes

--- app/views/modals/process-queue/priority-number-details-modal.blade.php
+++ app/views/modals/process-queue/priority-number-details-modal.blade.php
@@ -9,35 +9,33 @@
                 <h3 class="modal-title" id=""></h3>
             </div>
             <div class="modal-body">
-                <ul id="pmore-tab" class="nav nav-tabs nav-justified">
+                <ul id="pmore-tab" class="nav nav-tabs">
                     <li class="active"><a href="#details" data-toggle="tab">DETAILS</a></li>
                     <li><a href="#messages" data-toggle="tab" ng-click="getMessages()">MESSAGES</a></li>
                 </ul>
                 <div class="clearfix tab-content">
                     <div class="tab-pane fade active in" id="details">
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-5"><h5>Priority Number: </h5></div>
-                            <div class="col-md-6"><h5 id="priority-number-number"></h5></div>
+                        <div class="">
+                            <div class="col-md-6 col-xs-6"><h5>Priority Number: </h5></div>
+                            <div class="col-md-6 col-xs-6"><h5 id="priority-number-number"></h5></div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-5"><h5>Name: </h5></div>
-                            <div class="col-md-6"><h5 id="priority-number-name"></h5></div>
+                        <div class="">
+
+                            <div class="col-md-6 col-xs-6"><h5>Name: </h5></div>
+                            <div class="col-md-6 col-xs-6"><h5 id="priority-number-name"></h5></div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-5"><h5>Phone: </h5></div>
-                            <div class="col-md-6"><h5 id="priority-number-phone"></h5></div>
+                        <div class="">
+
+                            <div class="col-md-6 col-xs-6"><h5>Phone: </h5></div>
+                            <div class="col-md-6 col-xs-6"><h5 id="priority-number-phone"></h5></div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-1"></div>
-                            <div class="col-md-5"><h5>Email: </h5></div>
-                            <div class="col-md-6"><h5 id="priority-number-email"></h5></div>
+                        <div class="">
+                            <div class="col-md-6 col-xs-6"><h5>Email: </h5></div>
+                            <div class="col-md-6 col-xs-6"><h5 id="priority-number-email"></h5></div>
                         </div>
                     </div>
                     <div class="tab-pane fade in" id="messages">
-                        <div class="row">
+                        <div class="">
                             <div class="col-md-12 text-center"><h5>Conversation History</h5></div>
                             <div class="col-md-12" style="max-height: 300px; overflow: auto;">
                                 <div ng-repeat="message in messages">
@@ -61,7 +59,7 @@
                                 <h5>Send A Message</h5>
                                 Send to Phone: <input type="checkbox" ng-model="send_to_phone">
                                 <textarea class="form-control" rows="5" placeholder="Write a message..." style="resize: none;" ng-model="message_reply"></textarea>
-                                <button class="btn btn-primary btn-md pull-right" ng-click="sendBusinessReply()">SEND</button>
+                                <button class="btn btn-primary btn-md pull-right" ng-click="sendBusinessReply()">Send Message</button>
                             </div>
                         </div>
                     </div>
