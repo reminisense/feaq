05416f981ddd3a3dd1d1e9088d1a52d1cf93324a
Carl Dalid <carlodalid@gmail.com>
2015-06-15 14:28:17 +0800

    CSD Failed Include in Latest Commit 06152015 - file was updated after merge and wasn't included in latest commit

--- app/views/business/my-business-tabs/advertisements-tab.blade.php
+++ app/views/business/my-business-tabs/advertisements-tab.blade.php
@@ -1,32 +1,28 @@
-<div class="clearfix header">
-    <h5>ADVERTISEMENT</h5>
-    <small>Display advertisements on your broadcast screen. Choose either Internet TV or Image/Video</small>
-</div>
-
+    <div class="clearfix header">
+        <h5>ADVERTISEMENT</h5>
+        <small>Display advertisements on your broadcast screen. Choose either Internet TV or Image/Video</small>
+    </div>
     <div class="col-md-6 col-xs-12 col-sm-12">
         <div class="well" style="min-height:244px;">
             <h5>INTERNET TV
                 {{--<small>Internet TV can be viewed in the broadcast screen.</small>--}}
             </h5>
-
             <form ng-submit="selectTV(business_id)" id="tv-channel-select">
-                        <div class="form-group">
-                            <small>Choose channel:</small>
-                            <select ng-model="tv_channel" ng-init="tv_channel" id="tv-channel" class="form-control">
-                                <option value="">- Select A Channel -</option>
-                                @include('business.my-business-tabs.channels')
-                            </select>
-                            <div class="alert alert-success" id="tvchannel-success" style="display: none;">Success! <strong><a href="/broadcast/business/@{{ business_id }}" target="_blank">View Broadcast Page</a></strong></div>
-                            <div class="alert alert-danger" id="tvchannel-danger" style="display: none;">Oops! Something went wrong.</div>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-orange"  id="tv-script-submit-btn"><span class="glyphicon glyphicon-check"></span> SELECT</button>
-                            <button id="loading-img-3" style="display:none;" class="btn btn-orange btn-disabled"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...</button>
-                        </div>
+                <div class="form-group">
+                    <small>Choose channel:</small>
+                    <select ng-model="tv_channel" ng-init="tv_channel" id="tv-channel" class="form-control">
+                        <option value="">- Select A Channel -</option>
+                        @include('business.my-business-tabs.channels')
+                    </select>
+                    <div class="alert alert-success" id="tvchannel-success" style="display: none;">Success! <strong><a href="/broadcast/business/@{{ business_id }}" target="_blank">View Broadcast Page</a></strong></div>
+                    <div class="alert alert-danger" id="tvchannel-danger" style="display: none;">Oops! Something went wrong.</div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-orange"  id="tv-script-submit-btn"><span class="glyphicon glyphicon-check"></span> SELECT</button>
+                    <button id="loading-img-3" style="display:none;" class="btn btn-orange btn-disabled"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span> Loading...</button>
+                </div>
             </form>
         </div>
-    </div>
-    <div class="col-md-12 col-xs-12 col-sm-12">
         <div class="well">
             <h5>ANNOUNCEMENT TICKER
                 {{--<small>Internet TV can be viewed in the broadcast screen.</small>--}}
@@ -46,7 +42,6 @@
             </form>
         </div>
     </div>
-</div>
 
     <div class="col-md-6 col-xs-12 col-sm-12">
         <div class="well">
@@ -101,6 +96,8 @@
             </div>
         </div>
     </div>
+
+
 
 
 
