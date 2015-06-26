c906c80192c30b5dd409daf5616e40bf80db2b81
rdhdia <ruffy.heredia@gmail.com>
2015-06-26 18:48:04 +0800

    RDH Added new function to send using manual PHP functions - added /rest/send-manual/<token>/<message>/<title>/<subtitle>

--- app/controllers/RestController.php
+++ app/controllers/RestController.php
@@ -289,5 +289,47 @@ class RestController extends BaseController {
             ->send($message);
     }
 
+    public function getSendManual($device_token, $message, $title = "FeatherQ", $subtitle = null) {
+        // API access key from Google API's Console
+        define( 'API_ACCESS_KEY', 'AIzaSyCj0EfjXkZe-USRLOlTXxywayUXSIYg1wA' );
+
+        $registrationIds = array($device_token);
+
+        // prep the bundle
+        $msg = array
+        (
+            'message'       => $message,
+            'title'         => $title,
+            'subtitle'      => $subtitle,
+            'tickerText'    => $message,
+            'vibrate'   => 1,
+            'sound'     => 1
+        );
+
+        $fields = array
+        (
+            'registration_ids'  => $registrationIds,
+            'data'              => $msg
+        );
+
+        $headers = array
+        (
+            'Authorization: key=' . API_ACCESS_KEY,
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+        curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+        curl_setopt( $ch,CURLOPT_POST, true );
+        curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+        curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+        $result = curl_exec($ch );
+        curl_close( $ch );
+
+        echo $result;
+    }
+
 
 }
