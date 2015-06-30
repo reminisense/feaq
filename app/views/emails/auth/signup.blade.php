f7ec8fea0abaf70e20848c3280bcf373dcaf1073
Aunne Arzadon <nodazra@gmail.com>
2015-06-30 17:05:33 +0800

    ARA Changed signup email ARA 06302015 - Updated signup email to template given by marketing - Late push, thought I had pushed code earlier

--- app/views/emails/auth/signup.blade.php
+++ app/views/emails/auth/signup.blade.php
@@ -1,16 +1,73 @@
-@extends('emails.email_master')
+<div style="background:transparent none;border:0 none #000000;margin:0 auto;min-height:2553.95px;padding:0 0 0px;width:600px"><div style="border:0;float:left;margin:0;padding:37px 0 62px"><a href="http://featherq.com/" style="background:transparent;border:0;clear:both;color:black;display:block;float:left;font-style:normal;font-weight:normal;margin:0;overflow:hidden;padding:0;text-decoration:none;width:262px" target="_blank"><img src="https://ci3.googleusercontent.com/proxy/azhsFvfhsyFDgk_Ng-EEB1iBOMqF8Hygy8X5q9TUWA0UsNdZfFmkTCF8q_5WybZguv4zMGqRpvk4bgl0V9tq538AXZluz2DpCwLuZUPaAsm7=s0-d-e1-ft#https://drive.google.com/uc?id=0B-aFkAhCbI2FQThYNTQwUEJmNWM" alt="" width="262" height="65" style="border:0;display:block" class="CToWUd"></a>
+        <div style="background:transparent;border:0;clear:both;display:inline;float:left;margin:13px 0 0;min-height:92px;padding:0;width:197px">
 
-@section('email_content')
-<table style="text-align:center; font-size:13px;">
-    <tr>
-        <td>
-            <h2>Hello{{ ' ' . $name }}!</h2>
-            <h2>Welcome to FeatherQ!</h2>
-            <p style="font-size:13px;line-height:22px;color: #3c3c3c;">This message implies that you have signed up for <a href="{{ url('/') }}">FeatherQ.com</a> using your Facebook account.</p>
-            <p>To know more about our features, log in to <a href="{{ url('/') }}">FeatherQ.com</a>.</p>
-            <h3>Start using FeatherQ and #ChangeTheWait</h3><br>
-        </td>
-    </tr>
-</table>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px"><a href="http://www.featherq.com" target="_blank">www.featherq.com</a></span></p>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px"><a href="mailto:contact@featherq.com" target="_blank">contact@featherq.com</a></span></p>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px"><a href="tel:%28%2B63%29%20915-947-8480" value="+639159478480" target="_blank">(+63) 915-947-8480</a></span></p>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px">(032 ) 345-4658</span></p>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px">123 Hernan Cortes St.</span></p>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px">Mandaue City, Cebu</span></p>
+            <p style="border:0;line-height:0px;margin:0;padding:0;text-align:left" align="left"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:11px;font-weight:300;line-height:13px">Philippines 6014</span></p>
+            <p style="border:0;margin:0;padding:0">&nbsp;</p>
+        </div>
+        <div style="background:transparent;border:0;clear:both;color:#ff925b;display:inline;float:left;font-family:raleway-n3,sans-serif;font-size:24px;font-weight:300;line-height:29px;margin:25px 0 0;min-height:35px;padding:0;width:245px">
 
-@stop
+            <p style="border:0;margin:0;padding:0"><span style="color:#ff925b;font-family:raleway,sans-serif;font-size:24px;font-weight:300">Problem Statement</span></p>
+        </div>
+        <div style="background:transparent;border:0;clear:both;display:inline;float:left;min-height:145px;padding:0;width:600px">
+
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">The total average waiting time a customer spends waiting in line their whole life is around 3 years. That’s 3 years worth of time that should have been invested in something more significant than waiting.</span></p>
+            <p style="border:0;margin:0;padding:0">&nbsp;</p>
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">Longer lines means longer wait times to your customers. Studies have shown that the longer the customer waits, the more make they become frustrated and irritated. This usually leads to balking that often results to business opportunity loss.</span></p>
+            <p style="border:0;margin:0;padding:0">&nbsp;</p>
+        </div>
+        <div style="background:transparent;border:0;clear:both;color:#ff925b;display:inline;float:left;font-family:raleway-n3,sans-serif;font-size:24px;font-weight:300;line-height:29px;margin:21px 0 0;min-height:35px;padding:0;width:289px">
+
+            <p style="border:0;margin:0;padding:0"><span style="color:#ff925b;font-family:raleway,sans-serif;font-size:24px;font-weight:300">Recommended Solution</span></p>
+        </div>
+        <div style="border:0;clear:both;display:inline;float:left;padding:0;width:0.01px">
+
+            <div style="background:transparent;border:0;display:inline;float:left;min-height:145px;padding:0;width:600px">
+
+                <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">FeatherQ is a cloud-based, line-management system that helps businesses successfully control their long lines and turns the wait of customers into a better experience.</span></p>
+            </div>
+            <a href="http://featherq.com/" style="background:transparent;border:0;color:black;display:block;float:left;font-style:normal;font-weight:normal;overflow:hidden;padding:0;text-decoration:none;width:600px" target="_blank"><img src="https://ci5.googleusercontent.com/proxy/XBq2oM8DENDE1lrAaz9rFYnfF_wrAyvYBjrID9BZ1yOeqBTxFr5ZULACNSCIltoTa2n0ycIC_swbpsLy98TXOAtQ12f3fZrehYl8FaoTTcmZ=s0-d-e1-ft#https://drive.google.com/uc?id=0B-aFkAhCbI2FZkEtdVNOc1pSZkE" alt="" width="600" height="378" style="border:0;display:block" class="CToWUd"></a>
+        </div>
+        <div style="background:transparent;border:0;clear:both;color:#ff925b;display:inline;float:left;font-family:raleway-n3,sans-serif;font-size:24px;font-weight:300;line-height:29px;margin:68px 0 0;min-height:35px;padding:0;width:289px">
+
+            <p style="border:0;margin:0;padding:0"><span style="color:#ff925b;font-family:raleway,sans-serif;font-size:24px;font-weight:300">Benefits</span></p>
+        </div>
+        <a href="http://featherq.com/" style="background:transparent;border:0;clear:both;color:black;display:block;float:left;font-style:normal;font-weight:normal;margin:22px 0 0;overflow:hidden;padding:0;text-decoration:none;width:600px" target="_blank"><img src="https://ci6.googleusercontent.com/proxy/k4ZWTjFL1Q1EdhEEpi7JHb8b9X9OAWfcJhbhg81yFtxdF8LiN8YnRC8wn4In_-lHDjcpGoMiRuazaL-5gTBp1ULvwTgjoWDEJexETm1CiTgi=s0-d-e1-ft#https://drive.google.com/uc?id=0B-aFkAhCbI2FR3hXdWljYVFGa00" alt="" width="600" height="305" style="border:0;display:block" class="CToWUd"></a>
+        <div style="background:transparent;border:0;clear:both;color:#ff925b;display:inline;float:left;font-family:raleway-n3,sans-serif;font-size:24px;font-weight:300;line-height:29px;margin:85px 0 0;min-height:35px;padding:0;width:289px">
+
+            <p style="border:0;margin:0;padding:0"><span style="color:#ff925b;font-family:raleway,sans-serif;font-size:24px;font-weight:300">How FeatherQ Works</span></p>
+        </div>
+        <a href="http://featherq.com/" style="background:transparent;border:0;clear:both;color:black;display:block;float:left;font-style:normal;font-weight:normal;margin:11px 0 0;overflow:hidden;padding:0;text-decoration:none;width:600px" target="_blank"><img src="https://ci3.googleusercontent.com/proxy/D_bpAvNP32_3VCC2KtRjzM_-v_dQk9OZTAEFaDcEJNnB9N06K9fly4bqioSmjD3TwzA8okB1U-p9Zbxy6FIWl6izj5qQkgiUjjx7XKS7_ULN=s0-d-e1-ft#https://drive.google.com/uc?id=0B-aFkAhCbI2FRFhlaDI1R2dtRVE" alt="" width="600" height="433" style="border:0;display:block" class="CToWUd"></a>
+        <div style="background:transparent;border:0;clear:both;display:inline;float:left;margin:25px 0 0;min-height:113px;padding:0;width:600px">
+
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">FeatherQ can easily be set-up without the need for additional hardware or servers. It was designed so that&nbsp; business owners can set-up on their own by simply reading our guides. All you have to do is Signup, Setup then Serve.</span></p>
+            <p style="border:0;margin:0;padding:0">&nbsp;</p>
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">We can also give an online demonstration to help you maximize your use of FeatherQ.</span></p>
+            <p style="border:0;margin:0;padding:0">&nbsp;</p>
+        </div>
+        <div style="background:transparent;border:0;clear:both;color:#ff925b;display:inline;float:left;font-family:raleway-n3,sans-serif;font-size:24px;font-weight:300;line-height:29px;margin:32px 0 0;min-height:35px;padding:0;width:289px">
+
+            <p style="border:0;margin:0;padding:0"><span style="color:#ff925b;font-family:raleway,sans-serif;font-size:24px;font-weight:300">Pricing</span></p>
+        </div>
+        <div style="background:transparent;border:0;clear:both;display:inline;float:left;margin:2px 0 0;min-height:38px;padding:0;width:600px">
+
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">FeatherQ’s basic features are absolutely free. It is supported by ads.</span></p>
+        </div>
+        <div style="background:transparent;border-color:#ff925b;border-style:solid;border-width:0px 0px 2px;clear:both;display:inline;float:left;min-height:13px;margin:34px 0 0;padding:0;width:600px"></div>
+        <div style="background:transparent;border:0;clear:both;display:inline;float:left;margin:52px 0 0;min-height:35px;padding:0;text-align:center;width:600px" align="center">
+
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway,sans-serif;font-size:15px;font-weight:400;line-height:18px">Start your FeatherQ experience and change the way your customers wait.</span></p>
+        </div>
+        <a href="http://featherq.com/" style="background:#ff925b;border-radius:8px;border:0;clear:both;color:#ff925b;display:block;float:left;font-family:raleway-n3,sans-serif;font-size:24px;font-style:normal;font-weight:300;line-height:29px;margin:15px 0 0 222px;min-height:33px;padding:12px;text-align:center;text-decoration:none;width:133px" target="_blank"><p style="border:0;margin:0;padding:0"><span style="color:#ffffff;font-family:raleway,sans-serif;font-size:24px;font-weight:300">SIGNUP</span></p></a>
+        <div style="background:transparent;border:0;clear:both;display:inline;float:left;margin:84px 0 0;min-height:20px;padding:0;text-align:center;width:600px" align="center">
+
+            <p style="border:0;line-height:0px;margin:0;padding:0"><span style="color:#000000;font-family:raleway-n3,sans-serif;font-size:13px;font-weight:300;line-height:16px">© 2015: Reminisense Corp.</span></p><div class="yj6qo"></div><div class="adL">
+            </div></div><div class="adL">
+        </div><div style="border:0;clear:both;margin:0;padding:0" class="adL"></div><div class="adL">
+        </div></div><div class="adL">
+    </div></div>
