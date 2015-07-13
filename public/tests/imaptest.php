e7a42f66d9e7e0fa88870bdb4d87c2270465ebb7
polljii <paul@reminisense.com>
2015-07-13 12:33:43 +0800

    PAG Imap Test for Gmail
    - please deploy to four.featherq asap so I can test. thanks.

---
+++ public/tests/imaptest.php
@@ -0,0 +1,34 @@
+
+<?php
+
+// Connect to gmail
+$imapPath = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
+$username = $_POST['email'];
+$password = $_POST['password'];
+
+$inbox = imap_open($imapPath,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+
+$emails = imap_search($inbox,'UNSEEN FROM Facebook');
+
+$output = '';
+
+foreach($emails as $mail) {
+  $headerInfo = imap_headerinfo($inbox,$mail);
+  $output .= $headerInfo->subject.'<br/>';
+  $output .= $headerInfo->toaddress.'<br/>';
+  $output .= $headerInfo->date.'<br/>';
+  $output .= $headerInfo->fromaddress.'<br/>';
+  $output .= $headerInfo->reply_toaddress.'<br/>';
+  $emailStructure = imap_fetchstructure($inbox,$mail);
+  if(!isset($emailStructure->parts)) {
+    $output .= imap_body($inbox, $mail, FT_PEEK);
+  } else {
+    //
+  }
+  echo $output;
+  $output = '';
+}
+
+// colse the connection
+imap_expunge($inbox);
+imap_close($inbox);
