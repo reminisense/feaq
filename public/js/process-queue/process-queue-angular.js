28fe732f180708cda360b1db714918ab6cb6aa09
Jonas Clyde Almocera <jonas@reminisense.com>
2015-07-28 17:21:57 +0800

    JCA Change Rating Implementation 07282015 - Changed the served rating implementation so that the default rating is 3 - Changed the dropped rating implementation so that the rating for dropped numbers is 0.

--- public/js/process-queue/process-queue-angular.js
+++ public/js/process-queue/process-queue-angular.js
@@ -56,6 +56,7 @@
             angular.forEach($scope.temp_called_numbers, function(temp,i){
                 if(temp.tran_number == transaction_number){
                     if( temp.email_checker == true){
+                        temp.rating = (temp.rating ? temp.rating : 3) ;
                         $http.get(pq.urls.rating.ratings_url + temp.rating + "/" + temp.email + "/" + temp.terminal_id + '/' + 2)
                             .success(function(response){
                             });
@@ -75,7 +76,7 @@
             angular.forEach($scope.temp_called_numbers, function(temp,i){
                 if(temp.tran_number == transaction_number){
                     if( temp.email_checker == true){
-                        $http.get(pq.urls.rating.ratings_url + temp.rating + "/" + temp.email + "/" + temp.terminal_id + '/' + 3)
+                        $http.get(pq.urls.rating.ratings_url + 0 + "/" + temp.email + "/" + temp.terminal_id + '/' + 3)
                             .success(function(response){
                             });
                     }
@@ -87,7 +88,7 @@
         $scope.serveAndCallNext = function(transaction_number){
             $scope.serveNumber(transaction_number, function(){
                 $scope.issue_call_number = null;
-                $scope.callNumber();
+                $scope.issueOrCall();
             });
         };
 
