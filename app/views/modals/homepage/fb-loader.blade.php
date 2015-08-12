3bd56b179592f5a73f4643a363ccf0551dc7c932
carlodalid <carlodalid@gmail.com>
2015-08-12 12:49:03 +0800

    CSD Facebook Login Modal 08122015 - updated css styles for Facebook login modal - added margin and spacing

--- app/views/modals/homepage/fb-loader.blade.php
+++ app/views/modals/homepage/fb-loader.blade.php
@@ -2,9 +2,9 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
-                <img src="/img/fb-security.jpg" width="40px" alt="fb-security">
+                <img src="/img/fb-security.jpg" width="40px" style="margin-right: 15px;" alt="fb-security">
                 Contacting Facebook to verify your account. Please wait.
-                <img src="/img/fb-loader.gif" width="40px" alt="fb-loader">
+                <img src="/img/fb-loader.gif" width="40px" style="float: right; margin-top: 5px;" alt="fb-loader">
             </div>
         </div>
     </div>
