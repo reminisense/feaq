8043a62c610e95ff143e3122f8471a5f14886bb2
rodelmaranon <rodel.maranon@gmail.com>
2015-10-01 10:38:12 +0800

    RBM Fixed Modal popup for Websocket 10012015 - adjusted image width - added margin-right - justified the content

--- app/views/modals/websockets/websocket-loader.blade.php
+++ app/views/modals/websockets/websocket-loader.blade.php
@@ -1,9 +1,11 @@
 <div id="WebsocketLoaderModal" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-body">
-                <img src="/img/socket-loader.gif" style="margin-top: 5px;" alt="socket-loader">
+        <div class="modal-content" style="padding: 20px 0;">
+            <div class="modal-body" style="padding-top:10px;">
+                <div style="margin:auto;text-align: center;">
+                    <img  src="/img/socket-loader.gif" style="margin-top: 5px; margin-right: 20px; width:40px;" alt="socket-loader">
                 Establishing network connection with FeatherQ servers. Please wait.
+                </div>
             </div>
         </div>
     </div>
