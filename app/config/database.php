7a32de899f86a76c0530e3843d94a2cf6423dc20
Aunne Arzadon <nodazra@gmail.com>
2015-10-30 19:58:28 +0800

    ARA Updated to correct database password

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
             'host'      => 'localhost',
             'database'  => 'feaq',
             'username'  => 'forge',
-            'password'  => 'OjZcSiHUBoZJ2FGigqpv',
+            'password'  => 'jE8Gf9whVlHjXeIpCYiM',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
