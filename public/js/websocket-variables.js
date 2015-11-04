d16e45041a48cab4d8626ca2835fb1c01b90f80c
polljii <paul@reminisense.com>
2015-11-04 09:00:38 +0800

    PAG Pointed IP to Socket Server

--- public/js/websocket-variables.js
+++ public/js/websocket-variables.js
@@ -1,2 +1,2 @@
-var websocket_url = "ws://128.199.169.32:443/socket/server.php";
+var websocket_url = "ws://188.166.234.33:443/socket/server.php";
 //var websocket_url = "ws://localhost:443/socket/server.php";
