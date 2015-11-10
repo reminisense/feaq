2ceb93a5758de6853d53ee8fd6ae8a78fa6412bd
Nico Martin Enego <nmenego@gmail.com>
2015-11-10 17:52:07 +0800

    - add raw code generator - add raw code collision checker - add unit test methods - update code formatting of Helper.php - add utils to autoload classmap - add RandomStringGenertor utility

--- app/models/Helper.php
+++ app/models/Helper.php
@@ -1,4 +1,7 @@
 <?php
+
+use utils\RandomStringGenerator;
+
 /**
  * ARA - Helper class
  *
@@ -10,39 +13,70 @@
  * Date: 1/22/15
  * Time: 2:37 PM
  */
+class Helper extends Eloquent
+{
 
-class Helper extends Eloquent {
-
-  public static function VerifyFB($accessToken) {
-    // Call Facebook and let them verify if the information sent by the user
-    // is the same with the ones in their database.
-    // This will save us from the exploit of a post request with bogus details
-    $fb = new Facebook\Facebook(array(
-      'app_id' => '1577295149183234',
-      'app_secret' => '23a15a243f7ce66a648ec6c48fa6bee9',
-      'default_graph_version' => 'v2.4',
-    ));
-    try {
-      // Returns a `Facebook\FacebookResponse` object
-      $response = $fb->get('/me', $accessToken); // Use the access token retrieved by JS login
-      return $response;
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      //return json_encode(array('message' => $e->getMessage()));
-      Auth::logout();
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      //return json_encode(array('message' => $e->getMessage()));
-      Auth::logout();
+    public static function VerifyFB($accessToken)
+    {
+        // Call Facebook and let them verify if the information sent by the user
+        // is the same with the ones in their database.
+        // This will save us from the exploit of a post request with bogus details
+        $fb = new Facebook\Facebook(array(
+            'app_id' => '1577295149183234',
+            'app_secret' => '23a15a243f7ce66a648ec6c48fa6bee9',
+            'default_graph_version' => 'v2.4',
+        ));
+        try {
+            // Returns a `Facebook\FacebookResponse` object
+            $response = $fb->get('/me', $accessToken); // Use the access token retrieved by JS login
+            return $response;
+        } catch (Facebook\Exceptions\FacebookResponseException $e) {
+            //return json_encode(array('message' => $e->getMessage()));
+            Auth::logout();
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            //return json_encode(array('message' => $e->getMessage()));
+            Auth::logout();
+        }
     }
-  }
+
+    /**
+     * Generates random 4-character raw_code.
+     */
+    public static function generateRawCode()
+    {
+        // alphanumeric smallcaps
+        $customAlphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+        // Create new instance of generator class.
+        $generator = new RandomStringGenerator($customAlphabet);
+
+        // Set token length.
+        $tokenLength = 4;
+
+        // Call method to generate random string.
+        $raw_id = $generator->generate($tokenLength);
+
+        return $raw_id;
+    }
+
+    /**
+     * Returns true if raw_code exists in database.
+     * @param $rawCode
+     * @return bool
+     */
+    public static function isRawCodeExists($rawCode) {
+        $ret = Helper::firstFromTable('business', 'raw_code', $rawCode, '=');
+        return !empty($ret);
+    }
 
     /**
      * gets the user id of the current user
      * @return mixed
      */
-    public static function userId(){
-        if(Auth::check()){
+    public static function userId()
+    {
+        if (Auth::check()) {
             return Auth::user()->user_id;
-        }else{
+        } else {
             return 0;
         }
     }
@@ -51,7 +85,8 @@ class Helper extends Eloquent {
      * gets the role id of the current session's user
      * @return mixed
      */
-    public static function currentUserRoleId(){
+    public static function currentUserRoleId()
+    {
         return DB::table('user_role')->where('user_id', '=', Helper::userId())->first()->role_id;
     }
 
@@ -59,30 +94,35 @@ class Helper extends Eloquent {
      * checks if the role of the current user is in the given array
      * @return mixed
      */
-    public static function currentUserIsEither($roles = array()){
+    public static function currentUserIsEither($roles = array())
+    {
         return in_array(Helper::currentUserRoleId(), $roles);
     }
 
-    public static function parseTime($time){
+    public static function parseTime($time)
+    {
         $arr = explode(' ', $time);
         $hourmin = explode(':', $arr[0]);
 
         return [
             'hour' => trim($hourmin[0]),
-            'min'  => trim($hourmin[1]),
+            'min' => trim($hourmin[1]),
             'ampm' => trim($arr[1]),
         ];
     }
 
-    public static function mergeTime($hour, $min, $ampm){
-        return Helper::doubleZero($hour).':'.Helper::doubleZero($min).' '.$ampm;
+    public static function mergeTime($hour, $min, $ampm)
+    {
+        return Helper::doubleZero($hour) . ':' . Helper::doubleZero($min) . ' ' . $ampm;
     }
 
-    public static function doubleZero($number){
+    public static function doubleZero($number)
+    {
         return $number == 0 ? '00' : $number;
     }
 
-    public static function millisecondsToHMSFormat($ms){
+    public static function millisecondsToHMSFormat($ms)
+    {
         $second = $ms % 60;
         $ms = floor($ms / 60);
 
@@ -93,7 +133,8 @@ class Helper extends Eloquent {
         return Helper::formatTime($second, $minute, $hour);
     }
 
-    public static function formatTime($second, $minute, $hour){
+    public static function formatTime($second, $minute, $hour)
+    {
         $time_string = '';
         $time_string .= $hour > 0 ? $hour . ' hour(s) ' : '';
         $time_string .= $minute > 0 ? $minute . ' minute(s) ' : '';
@@ -101,15 +142,18 @@ class Helper extends Eloquent {
         return $time_string;
     }
 
-    public static function customSort($property, $var1, $var2){
+    public static function customSort($property, $var1, $var2)
+    {
         return $var1[$property] - $var2[$property];
     }
 
-    public static function customSortRev($property, $var1, $var2){
+    public static function customSortRev($property, $var1, $var2)
+    {
         return $var2[$property] - $var1[$property];
     }
 
-    public static function firstFromTable($table, $field, $value, $operator = '='){
+    public static function firstFromTable($table, $field, $value, $operator = '=')
+    {
         return DB::table($table)->where($field, $operator, $value)->first();
     }
 
@@ -119,13 +163,14 @@ class Helper extends Eloquent {
      * @param $conditions
      * @return mixed
      */
-    public static function getMultipleQueries($table, $conditions){
+    public static function getMultipleQueries($table, $conditions)
+    {
         $query = DB::table($table);
-        foreach($conditions as $field => $value){
+        foreach ($conditions as $field => $value) {
             $field = strpos($field, '.') > 0 ? substr($field, 0, strpos($field, '.')) : $field;
-            if(is_array($value)){
+            if (is_array($value)) {
                 $query->where($field, $value[0], $value[1]);
-            }else{
+            } else {
                 $query->where($field, '=', $value);
             }
         }
@@ -144,30 +189,32 @@ class Helper extends Eloquent {
         $tmp = getenv("HTTP_CLIENT_IP");
         // you DON'T want the HTTP_CLIENT_ID to equal unknown. That said, I don't
         // believe it ever will (same for all below)
-        if ( $tmp && !strcasecmp($tmp, "unknown"))
+        if ($tmp && !strcasecmp($tmp, "unknown"))
             return $tmp;
 
         $tmp = getenv("HTTP_X_FORWARDED_FOR");
-        if( $tmp && !strcasecmp($tmp, "unknown"))
+        if ($tmp && !strcasecmp($tmp, "unknown"))
             return $tmp;
 
         // no sense in testing SERVER after this.
         // $_SERVER[ 'REMOTE_ADDR' ] == gentenv( 'REMOTE_ADDR' );
         $tmp = getenv("REMOTE_ADDR");
-        if($tmp && !strcasecmp($tmp, "unknown"))
+        if ($tmp && !strcasecmp($tmp, "unknown"))
             return $tmp;
 
-        return("unknown");
+        return ("unknown");
     }
 
     /**
      * @param $birthdate must be int ex. strtotime(1/1/1990)
      */
-    public static function getAge($birthdate){
-        return floor( (time() - $birthdate) / 31556926);
+    public static function getAge($birthdate)
+    {
+        return floor((time() - $birthdate) / 31556926);
     }
 
-    public static function getTimezoneList(){
+    public static function getTimezoneList()
+    {
         $timezones = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
         return $timezones;
     }
@@ -179,8 +226,9 @@ class Helper extends Eloquent {
      * @param $user_timezone
      * @return string
      */
-    public static function changeBusinessTimeTimezone($date, $business_timezone, $browser_timezone){
-        if(is_numeric($browser_timezone)) $browser_timezone = Helper::timezoneOffsetToName($browser_timezone);
+    public static function changeBusinessTimeTimezone($date, $business_timezone, $browser_timezone)
+    {
+        if (is_numeric($browser_timezone)) $browser_timezone = Helper::timezoneOffsetToName($browser_timezone);
         $datetime = new DateTime($date, new DateTimeZone($business_timezone));
         $datetime->setTimezone(new DateTimeZone($browser_timezone));
         return $datetime->format('g:i A');
@@ -191,7 +239,8 @@ class Helper extends Eloquent {
      * @param $offset
      * @return bool
      */
-    public static function timezoneOffsetToName($offset){
+    public static function timezoneOffsetToName($offset)
+    {
         $abbrarray = timezone_abbreviations_list();
         foreach ($abbrarray as $abbr) {
             foreach ($abbr as $city) {
@@ -203,7 +252,8 @@ class Helper extends Eloquent {
         return false;
     }
 
-    public static function timezoneOffsetToNameArray($offset){
+    public static function timezoneOffsetToNameArray($offset)
+    {
         $timezones = [];
         $abbrarray = timezone_abbreviations_list();
         foreach ($abbrarray as $abbr) {
@@ -216,24 +266,27 @@ class Helper extends Eloquent {
         return $timezones;
     }
 
-  public static function isBusinessOwner($business_id, $user_id) {
-    return $business_id == UserBusiness::getBusinessIdByOwner($user_id);
-  }
-
-  public static function isPartOfBusiness($business_id, $user_id) {
-    $res = TerminalUser::getTerminalAssignement($user_id);
-    if (isset($res)) {
-      foreach ($res as $count => $data) {
-        if ($business_id == Business::getBusinessIdByTerminalId($data['terminal_id'])) {
-          return TRUE;
-        }
-      }
+    public static function isBusinessOwner($business_id, $user_id)
+    {
+        return $business_id == UserBusiness::getBusinessIdByOwner($user_id);
     }
-    return FALSE;
-  }
 
-  public static function isNotAnOwner($user_id) {
-    return !UserBusiness::getBusinessIdByOwner($user_id);
-  }
+    public static function isPartOfBusiness($business_id, $user_id)
+    {
+        $res = TerminalUser::getTerminalAssignement($user_id);
+        if (isset($res)) {
+            foreach ($res as $count => $data) {
+                if ($business_id == Business::getBusinessIdByTerminalId($data['terminal_id'])) {
+                    return TRUE;
+                }
+            }
+        }
+        return FALSE;
+    }
+
+    public static function isNotAnOwner($user_id)
+    {
+        return !UserBusiness::getBusinessIdByOwner($user_id);
+    }
 
 }
