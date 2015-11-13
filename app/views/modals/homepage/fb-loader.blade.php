8b3202bde21aba775e38b731757d7cedb42e1830
rodelmaranon <rodel.maranon@gmail.com>
2015-11-13 13:56:26 +0800

    RBM Users Landing Responsive Fix 11132015 - Fixed the main navigation - Made images responsive on mobiles devices - Added animations - lots of css changes

--- app/views/modals/homepage/fb-loader.blade.php
+++ app/views/modals/homepage/fb-loader.blade.php
@@ -1,10 +1,16 @@
-<div id="FBLoaderModal" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false">
+<div id="FBLoaderModal" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false" style="text-align: center;">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-body">
-                <img src="/img/fb-security.jpg" width="40px" style="margin-right: 15px;" alt="fb-security">
-                Contacting Facebook to verify your account. Please wait.
-                <img src="/img/fb-loader.gif" width="40px" style="float: right; margin-top: 5px;" alt="fb-loader">
+            <div class="modal-body clearfix">
+                <div class="col-md-1 col-sm-2 col-md-12">
+                    <img src="/img/fb-security.jpg" width="40px" style="margin-right: 15px;" alt="fb-security">
+                </div>
+                <div class="col-md-10 col-sm-8 col-md-12">
+                    <p class="col-xs-12">Contacting Facebook to verify your account. Please wait.</p>
+                </div>
+                <div class="col-md-1 col-sm-2 col-md-12" style=" margin:auto;">
+                    <img src="/img/fb-loader.gif" width="40px" style="float: right; text-align: center; margin-top: 5px;" alt="fb-loader">
+                </div>
             </div>
         </div>
     </div>
--- app/views/user/user-landing.blade.php
+++ app/views/user/user-landing.blade.php
@@ -24,6 +24,7 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/homepage/main.css">
     <link rel="stylesheet" href="/css/homepage/animate.css">
+    <link rel="stylesheet" href="/css/homepage/responsive.css">
     <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 
     <link rel="apple-touch-icon" sizes="57x57" href="/images/homepage/landing/apple-icon-57x57.png">
@@ -59,7 +60,7 @@
     <script type="text/javascript" src="/js/user/Usertracker.js"></script>
 
 </head>
-<body ng-app="FeatherQ" ng-cloak>
+<body id="user-landing" ng-app="FeatherQ" ng-cloak>
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
@@ -87,7 +88,7 @@
 <section id="banner" ng-controller="fbController">
     <div class="container">
         <div class="col-md-6">
-            <h1>Don't Waste Your Life -- Waiting</h1>
+            <h1>Don't Waste Your Life &mdash; Waiting</h1>
             <h2 class="sub-heading">Use FeatherQ's mobile application to join lines from anywhere, using your mobile device.</h2>
             <div class="cta">
                 <a href="https://play.google.com/store/apps/details?id=com.reminisense.featherq" target="_blank"><img alt="Android app on Google Play" src="https://developer.android.com/images/brand/en_app_rgb_wo_60.png"></a>
@@ -100,54 +101,56 @@
 
 <section id="find-a-business" ng-controller="searchBusinessCtrl">
     <div class="container">
-        <div class="col-md-12">
+        <div class="col-md-12 wow fadeInUp">
             <h2 class="text-center">Find a Business</h2>
         </div>
-        <div class="filterwrap col-md-offset-2 col-md-8">
-            <div class="row">
-                <div class="col-md-2 col-sm-2 col-xs-4 btn-group">
-                    <button id="btnGroupDrop1" type="button" class="btn btn-default dropdown-toggle ng-binding" data-toggle="dropdown">
-                        @{{ location_filter }}
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu" aria-labelledby="btnGroupDrop1" id="location-filter">
-                        <li ng-repeat="location in locations" ng-click="locationFilter(location.code);"><a href="">@{{ location.code }}</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-2 col-sm-2 col-xs-4 btn-group">
-                    <button id="btnGroupDrop1" type="button" class="btn btn-default dropdown-toggle ng-binding" data-toggle="dropdown">
-                        @{{ industry_filter }}
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu" aria-labelledby="btnGroupDrop1">
-                        <li ng-repeat="industry in industries" ng-click="industryFilter(industry.code);"><a href="">@{{ industry.code }}</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-2 col-sm-2 col-xs-4 btn-group">
-                    <input type="text" id="time_open-filter" name="time_open" ng-model="time_open" placeholder="Time Open" class="timepicker form-control">
-                </div>
-                <form class="ng-pristine ng-valid col-md-4 col-sm-4 col-xs-6">
-                    <div class="clearfix" style="position: relative">
-                        <input class="" type="text" placeholder="ie: Ng Khai Devt Corp" id="search-keyword" ng-model="search_keyword" ng-model-options="{debounce: 1000}" autocomplete="off" >
-                        <ul class="dropdown-menu" role="menu" id="search-suggest" ng-hide="dropdown_businesses.length == 0"  outside-click="dropdown_businesses = []">
-                            <li ng-repeat="business in dropdown_businesses">
-                                <a href="#" ng-click="searchBusiness(location_filter, industry_filter, business.name, $event)">
-                                    <strong class="business-name">@{{ business.name }}</strong><br>
-                                    <small class="address">@{{ business.local_address }}</small>
-                                </a>
-                            </li>
+        <div class="col-md-12">
+            <div class="filterwrap wow fadeInUp col-md-offset-2 col-md-8">
+                <div class="row">
+                    <div class="col-md-2 col-sm-2 col-xs-4 btn-group">
+                        <button id="btnGroupDrop1" type="button" class="btn btn-default dropdown-toggle ng-binding" data-toggle="dropdown">
+                            @{{ location_filter }}
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="btnGroupDrop1" id="location-filter">
+                            <li ng-repeat="location in locations" ng-click="locationFilter(location.code);"><a href="">@{{ location.code }}</a></li>
                         </ul>
                     </div>
-                </form>
-                <div class="col-md-2 col-sm-2 col-xs-6">
-                    <button type="button" class=" btn btn-black btn-md" ng-click="searchBusiness(location_filter, industry_filter, search_keyword);">SEARCH</button>
+                    <div class="col-md-2 col-sm-2 col-xs-4 btn-group">
+                        <button id="btnGroupDrop1" type="button" class="btn btn-default dropdown-toggle ng-binding" data-toggle="dropdown">
+                            @{{ industry_filter }}
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="btnGroupDrop1">
+                            <li ng-repeat="industry in industries" ng-click="industryFilter(industry.code);"><a href="">@{{ industry.code }}</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-xs-4 btn-group">
+                        <input type="text" id="time_open-filter" name="time_open" ng-model="time_open" placeholder="Time Open" class="timepicker form-control">
+                    </div>
+                    <form class="ng-pristine ng-valid col-md-4 col-sm-4 col-xs-6">
+                        <div class="clearfix" style="position: relative;background-color: #fff; border-bottom-left-radius: 4px;">
+                            <input class="" type="text" placeholder="ie: Ng Khai Devt Corp" id="search-keyword" ng-model="search_keyword" ng-model-options="{debounce: 1000}" autocomplete="off" >
+                            <ul class="dropdown-menu" role="menu" id="search-suggest" ng-hide="dropdown_businesses.length == 0"  outside-click="dropdown_businesses = []">
+                                <li ng-repeat="business in dropdown_businesses">
+                                    <a href="#" ng-click="searchBusiness(location_filter, industry_filter, business.name, $event)">
+                                        <strong class="business-name">@{{ business.name }}</strong><br>
+                                        <small class="address">@{{ business.local_address }}</small>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </form>
+                    <div class="col-md-2 col-sm-2 col-xs-6">
+                        <button type="button" class=" btn btn-black btn-md" ng-click="searchBusiness(location_filter, industry_filter, search_keyword);">SEARCH</button>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="clearfix col-md-12" id="search-loader" style="display: none; text-align: center;">
             <img style="width: 41px;" src="/images/loader-spinner-white.gif" />
         </div>
-        <div class="clearfix" id="search-grid" style="display: none;">
+        <div class="clearfix wow fadeIn" id="search-grid" style="display: none;">
             <div class="col-md-3 ng-scope" ng-repeat="business in businesses">
                 <a class="business_link" href="/broadcast/business/@{{ business.business_id }}" target="_blank">
                     <div class="box-wrap">
@@ -163,21 +166,21 @@
 <section id="wait">
     <div class="container">
         <div class="col-md-12">
-            <h2 class="text-center">Wait on your own terms</h2>
+            <h2 class="wow fadeInUp text-center">Wait on your own terms</h2>
         </div>
-        <div class="col-md-4 col-sm-4 text-center">
-            <img class="wow fadeInUp img-responsive" src="/images/homepage/landing/wait1.png" alt="Join the line from anywhere" />
+        <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/wait1.png" alt="Join the line from anywhere" />
             <h4 class="orange">Join the line. From Anywhere.</h4>
             <p>Use FeatherQ to join lines using your smartphone or mobile device. Get into the queue even before you arrive at the store.</p>
         </div>
-        <div class="col-md-4 col-sm-4 text-center">
-            <img class="wow fadeInUp" src="/images/homepage/landing/wait2.png" alt="Freedom away from the line." />
+        <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/wait2.png" alt="Freedom away from the line." />
             <h4 class="orange">Freedom away from the line.</h4>
             <p>Hate getting stuck while you wait? Shop, dine and explore-- instead of waiting in one spot.
                 Receive real-time updates about your queue status and notifications when you should get back.</p>
         </div>
-        <div class="col-md-4 col-sm-4 text-center">
-            <img class="wow fadeInUp" src="/images/homepage/landing/wait3.png" alt="Share your experience." />
+        <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/wait3.png" alt="Share your experience." />
             <h4 class="orange">Share your experience.</h4>
             <p>Share your experiences in line with friends and help them discover a better way to wait.</p>
         </div>
@@ -187,22 +190,22 @@
 <section id="featherq-features">
     <div class="container">
         <div class="col-md-12">
-            <h2 class="text-center">FeatherQ Features</h2>
+            <h2 class="wow fadeInUp text-center">FeatherQ Features</h2>
         </div>
-        <div class="col-md-6 col-sm-6 text-center">
-            <img class="wow fadeInUp img-responsive" src="/images/homepage/landing/feature3.png" alt="Join the line from anywhere" />
+        <div class="col-md-6 col-sm-6 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/feature3.png" alt="Join the line from anywhere" />
             <p>Our geo-locator will help you find the nearest businesses. <br>FeatherQ lets you see the status of their lines.</p>
         </div>
-        <div class="col-md-6 col-sm-6 text-center">
-            <img class="wow fadeInUp img-responsive" src="/images/homepage/landing/feature2.png" alt="Join the line from anywhere" />
+        <div class="col-md-6 col-sm-6 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/feature2.png" alt="Join the line from anywhere" />
             <p>Join the line virtually using the FeatherQ app. <br>With FeatherQ, you can join lines using your smartphone before you even reach the store.</p>
         </div>
-        <div class="col-md-6 col-sm-6 text-center">
-            <img class="wow fadeInUp img-responsive" src="/images/homepage/landing/feature1.png" alt="Join the line from anywhere" />
+        <div class="col-md-6 col-sm-6 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/feature1.png" alt="Join the line from anywhere" />
             <p>Receive push notifications on our mobile app<br> whenever your turn is up.</p>
         </div>
-        <div class="col-md-6 col-sm-6 text-center">
-            <img class="wow fadeInUp img-responsive" src="/images/homepage/landing/feature4.png" alt="Join the line from anywhere" />
+        <div class="col-md-6 col-sm-6 text-center wow fadeInUp">
+            <img class="img-responsive" src="/images/homepage/landing/feature4.png" alt="Join the line from anywhere" />
             <p>Rate your lining experience with different <br>establishments and share them with your friends!</p>
         </div>
     </div>
@@ -211,18 +214,15 @@
 <section id="beat" ng-controller="fbController">
     <div class="container text-center">
         <div class="col-md-12">
-            <h2 class="text-center">Beat the waiting game</h2>
+            <h2 class="wow fadeInUp text-center">Beat the waiting game</h2>
         </div>
-        <div class="col-md-offset-2 col-md-8 text-center">
+        <div class="col-md-offset-2 col-md-8 text-center wow fadeInUp">
             <p>FeatherQ is free to use. Forever. All you need to start queuing is a Facebook account, and an internet connection.</p>
-
             <p>Get started now. Download the app  and login using your Facebook account and discover a better way to wait.</p>
-
             <div class="cta">
                 <a href="https://play.google.com/store/apps/details?id=com.reminisense.featherq" target="_blank"><img alt="Android app on Google Play" src="https://developer.android.com/images/brand/en_app_rgb_wo_60.png"></a>
                 <a href="#" ng-click="login()" class="btn btn-blue"><img src="/images/homepage/landing/fb.png">LOGIN WITH FACEBOOK</a>
             </div>
-
             <p class="black">Looking to manage your own line?<p>
                 <a href="/business">Try FeatherQ for Business</a>
         </div>
