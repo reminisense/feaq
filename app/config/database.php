67c5be0bdff0dca8c91aea9d73872a9f17e48558
polljii <paul@reminisense.com>
2016-01-07 18:47:01 +0800

    database.php file for demo.featherq.com

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
             'host'      => 'localhost',
             'database'  => 'feaq',
             'username'  => 'forge',
-            'password'  => 'TkLKr5ysCF9DQfJuVzx7',
+            'password'  => 'OjZcSiHUBoZJ2FGigqpv',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
