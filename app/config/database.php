81dc7c282e950ac8e5ab442da15d2afde9105d14
rodelmaranon <rodel.maranon@gmail.com>
2016-03-07 17:07:55 +0800

    FQW-130 03072016 Font changes - Made the service and terminal names bigger and bolder

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => 'localhost',
+            'host'      => '127.0.0.1',
             'database'  => 'feaq',
-            'username'  => 'forge',
-            'password'  => 'TkLKr5ysCF9DQfJuVzx7',
+            'username'  => 'root',
+            'password'  => '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
