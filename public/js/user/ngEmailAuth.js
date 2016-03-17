10f20d0483136294ebd645b453467d53215c1f3b
Jonas Clyde Almocera <jonas@reminisense.com>
2016-03-17 14:25:18 +0800

    JCA-Master_Admin_Messages_Quickfixes_2 03172016 -removed loader after resending message.

--- public/js/user/ngEmailAuth.js
+++ public/js/user/ngEmailAuth.js
@@ -39,6 +39,7 @@ app.controller('emailAuthController', function($scope, $http){
         $http.get('/user/resend-confirmation/'+email).success(function(response){
             $scope.error_message = 'Confirmation email was sent to your email.';
             $('#err_message').hide();
+            $('#resend-loader').hide();
         });
     }
 
