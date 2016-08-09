43a566b841856614012f9d258a0453584a2437ca
Polljii143 <paul@reminisense.com>
2016-08-09 16:26:36 +0800

    PAG Change Broadcast Filters to Navbar
    - changed implementation from selectbox to navbar tabs when choosing service to display

--- app/views/broadcast/default/business-ads-master.blade.php
+++ app/views/broadcast/default/business-ads-master.blade.php
@@ -1,12 +1,12 @@
 <div class="ads-area {{ $ad_class }} abs">
   <div class="top">
-      <div class="col-md-3 pull-right">
-          <select class="form-control" id="show-only-service">
-              <option value="0">ALL</option>
+      <div class="col-md-9 pull-right">
+          <ul class="nav nav-pills nav-justified pull-right" id="show-only-service" style="margin-top: -4px;">
+              <li role="presentation" class="service-filter-0" service_id="0"><a href="#">ALL</a></li>
               <?php foreach ($service_filters as $count => $service): ?>
-                <option value="{{ $service->service_id; }}">{{ $service->name; }}</option>
+                  <li role="presentation" class="service-filter-{{ $service->service_id; }}" service_id="{{ $service->service_id; }}"><a href="#">{{ $service->name; }}</a></li>
               <?php endforeach; ?>
-          </select>
+          </ul>
       </div>
       <a class="" href="#"><img src="/images/featherq-home-logo.png"></a>
   </div>
