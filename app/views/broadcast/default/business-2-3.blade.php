e7b9d40adeb0426f460ef7f2eddc8abeb00d3c2f
Polljii143 <paul@reminisense.com>
2016-09-14 20:02:40 +0800

    PAG Blinking is Now Box Instead of Numbers

--- app/views/broadcast/default/business-2-3.blade.php
+++ app/views/broadcast/default/business-2-3.blade.php
@@ -5,11 +5,11 @@
         <h3 class="abs">{{ $business_name }}</h3>
         <div class="row-3">
             <div class="col-1">
-                <div class="numbers t@{{ rank1 }} @{{ color1 }}">
-                    <p class="service blink-num">@{{ service1 }}</p>
-                    <p class="terminal blink-num">@{{ name1 }}</p>
-                    <p class="callnum blink-num">@{{ box1 }}</p>
-                    <p class="terminal blink-num">@{{ user1 }}</p>
+                <div class="numbers t@{{ rank1 }} @{{ color1 }} blink-num">
+                    <p class="service">@{{ service1 }}</p>
+                    <p class="terminal">@{{ name1 }}</p>
+                    <p class="callnum">@{{ box1 }}</p>
+                    <p class="terminal">@{{ user1 }}</p>
                 </div>
             </div>
             <div class="col-1">
