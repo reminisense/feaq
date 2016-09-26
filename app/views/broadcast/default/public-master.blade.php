42c2d583b310adb31930090791149df258e63169
Jonas Clyde Almocera <jonas@reminisense.com>
2016-09-26 16:41:45 +0800

    JCA-Datepicker_Bug 09262016 -Added datepicker js files.

--- app/views/broadcast/default/public-master.blade.php
+++ app/views/broadcast/default/public-master.blade.php
@@ -21,8 +21,10 @@
     <link href="/css/broadcast/default/responsive.css" rel="stylesheet" type="text/css" media="all">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+    <link href="/css/jquery-ui.min.css" rel="stylesheet" type="text/css" media="all">
 
     <script src="/js/jquery-1.11.2.min.js"></script>
+    <script src="/js/jquery-ui.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
     <script type="text/javascript" src="/js/jquery.marquee.min.js"></script>
     <script src="/js/ngFacebook.js"></script>
