c09faabe80d7cabd2f260d71540fb4dfc550afb8
Aunne Arzadon <nodazra@gmail.com>
2016-11-28 16:24:05 +0800

    ARA Priority number notes ARA 11282016 Added notes when issuing priority numbers

--- app/models/FreeApi/FreeQueue.php
+++ app/models/FreeApi/FreeQueue.php
@@ -12,6 +12,7 @@ class FreeQueue{
      * Issue a number to the service
      * @param $data[service_id]
      * @param $data[priority_number]
+     * @param $data[note]
      * @return string
      */
     public function issueNumber($data){
@@ -21,6 +22,7 @@ class FreeQueue{
         else{
             //$business_id = Business::getBusinessIdByServiceId($data['service_id']);
             $number = ProcessQueue::issueNumber($data['service_id'], $data['priority_number']);
+            if(isset($data['note'])){ $this->saveNote($number['transaction_number'], $data['note']); }
             //ProcessQueue::updateBusinessBroadcast($business_id);
             return json_encode(['success' => 1, 'number' => $number]);
         }
@@ -63,5 +65,9 @@ class FreeQueue{
     public function dropNumber($transaction_number){
         TerminalTransaction::where('transaction_number', '=', $transaction_number)->update(['time_removed' => time()]);
         return json_encode(['success' => 1]);
+    }
+
+    private function saveNote($transaction_number, $note){
+        PriorityQueue::where('transaction_number', '=', $transaction_number)->update(['note' => $note]);
     }
 }
