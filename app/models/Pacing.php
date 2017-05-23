d5f887eb13f9019f224d561384a9d238f5a52b92
lynnard <lynnard@reminisense.com>
2017-05-23 14:47:19 +0800

    Created Pacing model

--- app/models/Pacing.php
+++ app/models/Pacing.php
@@ -15,4 +15,24 @@ class Pacing extends Eloquent
     public static function addSlots($values = array()) {
         Pacing::insert($values);
     }
+
+    public static function create_record($service_id, $quota, $time_start, $time_end){
+        $pacing_record = [
+            'service_id' => $service_id,
+            'quota' => $quota,
+            'time_start' => $time_start,
+            'time_end' => $time_end
+        ];
+
+        Pacing::insertGetId($pacing_record);
+    }
+
+    public static function fetch_records($service_id){
+        return Pacing::where('service_id', $service_id)->get();
+    }
+
+
+    public static function delete_record($pacing_id){
+        return Pacing::where('pacing_id', '=', $pacing_id)->delete();
+    }
 }
